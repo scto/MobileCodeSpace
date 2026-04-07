091b03d56620946f12d2d2f66b3425de8a8ad45c
Thomas Schmid <1441369+scto@users.noreply.github.com>
2026-04-07 20:08:12 +0200

    Add base URLs for various architectures

--- app/src/main/java/com/mobilecodespace/app/MCSConstants.kt
+++ app/src/main/java/com/mobilecodespace/app/MCSConstants.kt
@@ -8,4 +8,20 @@ object MCSConstants {
     const val EDITOR_PROPS_FILE = "$EDITOR_PROPS_PATH/open_files.json"
     const val EDITOR_PROPS_FILE_BAK = "$EDITOR_PROPS_FILE.bak"
     const val MOBILECODESPACE_HOME = "MobileCodeSpace"
+
+    private const val BASE_URL = "https://raw.githubusercontent.com/Xed-Editor/Karbon-PackagesX/main"
+
+    const val PROOT_ARM = "$BASE_URL/arm/proot"
+    const val PROOT_ARM64 = "$BASE_URL/aarch64/proot"
+    const val PROOT_X64 = "$BASE_URL/x86_64/proot"
+
+    const val TALLOC_ARM = "$BASE_URL/arm/libtalloc.so.2"
+    const val TALLOC_ARM64 = "$BASE_URL/aarch64/libtalloc.so.2"
+    const val TALLOC_X64 = "$BASE_URL/x86_64/libtalloc.so.2"
+
+    private const val ROOTFS_BASE = "https://github.com/Xed-Editor/Karbon-PackagesX/releases/download/ubuntu"
+
+    const val ROOTFS_ARM = "$ROOTFS_BASE/ubuntu-base-24.04.3-base-armhf.tar.gz"
+    const val ROOTFS_ARM64 = "$ROOTFS_BASE/ubuntu-base-24.04.3-base-arm64.tar.gz"
+    const val ROOTFS_X64 = "$ROOTFS_BASE/ubuntu-base-24.04.3-base-amd64.tar.gz"
 }
